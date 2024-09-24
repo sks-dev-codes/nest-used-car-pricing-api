e2dd0419c5ba414875df61ff25b0a58868907a9b
Saurabh Sharma <saurabh.sharma232@outlook.com>
2024-09-24 08:42:21 +0000

    added entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -11,7 +13,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     database: 'nodeDB',
     password: 'password',
     username: 'root',
-    entities: [],
+    entities: [User, Report],
     synchronize: true
   }), UsersModule, ReportsModule],
   controllers: [AppController],
