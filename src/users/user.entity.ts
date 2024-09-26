9df6acaf40758adfbb25b81e467e4418714ababf
Saurabh Sharma <saurabh.sharma232@outlook.com>
2024-09-26 12:10:19 +0000

    added hooks

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterRemove, AfterUpdate } from "typeorm";
 
 @Entity()
 export class User {
@@ -8,4 +8,16 @@ export class User {
     email: string;
     @Column()
     password: string;
+    @AfterInsert()
+    logInsert() {
+        console.log('Inserted user with id ' + this.id);
+    }
+    @AfterRemove()
+    logRemove() {
+        console.log('Removed user with id ' + this.id);
+    }
+    @AfterUpdate()
+    logUpdate() {
+        console.log('Updated user with id ' + this.id)
+    }
 }
