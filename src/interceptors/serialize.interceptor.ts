4f60239efc53a8b14d1a748e2645078af951ec74
Saurabh Sharma <saurabh.sharma232@outlook.com>
2024-10-28 10:07:54 +0000

    signup and signin

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -3,19 +3,23 @@ import { plainToClass } from "class-transformer";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators"
 
-export function Serialize(dto: any){
+interface ClassConstructor {
+    new(...args: any[]): {}
+}
+
+export function Serialize(dto: ClassConstructor) {
     return UseInterceptors(new SerializeInterceptor(dto));
 }
 
 export class SerializeInterceptor implements NestInterceptor {
-    constructor(private dto: any){}
+    constructor(private dto: any) { }
     intercept(context: ExecutionContext, handler: CallHandler<any>): Observable<any> {
         //Run something before a request is handled by the request handler
         //console.log('I am running before the handler', context);
-        return handler.handle().pipe(map((data: any)=>{
+        return handler.handle().pipe(map((data: any) => {
             //Run something before the response is sent out
             //console.log('Im running before the response is sent out', data);
-            return plainToClass(this.dto, data, {excludeExtraneousValues: true})
+            return plainToClass(this.dto, data, { excludeExtraneousValues: true })
         }));
     }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,19 +2,29 @@ import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundExcep
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
-import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
-    constructor(private usersService: UsersService) { }
+    constructor(
+        private usersService: UsersService,
+        private authService: AuthService
+    ) { }
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
-        this.usersService.create(body.email, body.password);
+        return this.authService.signup(body.email, body.password);
     }
 
+    @Post('/signin')
+    signin(@Body() body: CreateUserDto) {
+        return this.authService.signin(body.email, body.password);
+    }
+
+    //@UseInterceptors(new SerializeInterceptor(UserDto))
     @Get('/:id')
-    @UseInterceptors(new SerializeInterceptor(UserDto))
     async findUser(@Param('id') id: string) {
         console.log('handller is running');
         const user = await this.usersService.findOne(parseInt(id));
